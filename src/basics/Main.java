02a138373a63087e81323ce699f2e5605fd87ee0
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-06 18:52:43 +0600

    understand the basics of java program

---
+++ src/basics/Main.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class Main {
+	static void main() {
+		System.out.println("Hello World!");
+	}
+}
+/*
+	public: public is an access modifier in java
+	class: class is a user defined blueprint with some attributes and behaviours
+	Main: Name of the class
+	static: static allows to access any property or method without creating Object
+	void: void is a return type. void means it didn't return anything
+	main(): main a function which is the entry point of the program
+	System.out.println: print a line
+*/
