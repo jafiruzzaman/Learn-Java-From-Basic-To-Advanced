4926f200716fe628b1e0eda92e03a200f1d64e1a
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-06 21:37:20 +0600

    2D Array initialization,accessing Elements,Iteration

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -33,5 +33,44 @@ public class Main {
 		for (int item:declareArray){
 			System.out.print(item+" ");
 		}
+
+		/*
+		 2D Array: A 2D Array is basically an Array of Array Think like as table with row and column
+		 1 2 3 4
+		 5 6 7 8
+		 9 10 11 12
+		 */
+		// Initialize a 2D Array
+		int[][] twoDArr;
+		// Declare a 2D Array
+		int[][] twoDArr2 = new int[3][4];
+		// Declare a 2D Array With Values
+		int[][] matrix = {
+			{1,2,3,4},
+			{5,6,7,8},
+			{9,10,11,12}
+		};
+
+		System.out.println();
+		// Accessing 2D Array Elements
+		System.out.println(matrix[1][2]);
+
+		// Iterating on 2D Array Using Normal For loop
+		System.out.println("Print 2D Array Using Normal For Loop");
+		for (int i=0;i< matrix.length;i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("Using For Each Loop");
+		// Iterating Using For-Each Loop
+		for(int[] row:matrix){
+			for (int col : row){
+				System.out.print(col+" ");
+			}
+			System.out.println();
+		}
+
 	}
 }
